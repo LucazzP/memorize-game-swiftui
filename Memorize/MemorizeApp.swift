b3fc519b1dbf93ff571bcf3bb62e30bcd66f320a
LucaszzP <lucashpolazzo2013@gmail.com>
2021-05-22 19:46:58 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Lucas Polazzo on 5/22/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
